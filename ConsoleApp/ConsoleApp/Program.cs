6dc7c7207e684b70826ce2a7b3295a9f7cf70adc
jobeland <jordan.beland@gmail.com>
2016-05-04 13:57:39 -0700

    upload string to blob

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -15,7 +15,29 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             var connectionString = File.ReadAllText("C:\\ConnectionStrings\\StorageAccountConnectionString.txt");
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString); 
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+
+            // Create the container if it doesn't already exist.
+            container.CreateIfNotExists();
+
+            // Retrieve reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("myblob");
+
+            // Create or overwrite the "myblob" blob with contents from a local file.
+            using (var stream = GenerateStreamFromString("this is some test blob content"))
+            {
+                blockBlob.UploadFromStream(stream);
+            }
+        }
+
+        private static MemoryStream GenerateStreamFromString(string value)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(value ?? ""));
         }
     }
 }
