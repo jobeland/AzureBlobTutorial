26cbf31c1c8cbb71dd013840de6a27f207f3e4ba
jobeland <jordan.beland@gmail.com>
2016-05-04 12:13:02 -0700

    add connection string reader

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
 
 namespace ConsoleApp
 {
@@ -10,6 +14,8 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            var connectionString = File.ReadAllText("C:\\ConnectionStrings\\StorageAccountConnectionString.txt");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString); 
         }
     }
 }
