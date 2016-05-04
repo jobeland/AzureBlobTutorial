9357d5621453b9a11afa37f2cab18e5c19081ba5
jobeland <jordan.beland@gmail.com>
2016-05-04 14:08:34 -0700

    list blobs

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,7 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure; // Namespace for CloudConfigurationManager
 using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
-using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Blob.Protocol;
+
+// Namespace for Blob storage types
 
 namespace ConsoleApp
 {
@@ -16,8 +19,21 @@ namespace ConsoleApp
         {
             var connectionString = File.ReadAllText("C:\\ConnectionStrings\\StorageAccountConnectionString.txt");
 
+            UploadToBlobs(connectionString);
+
+            ListBlobs(connectionString);
+
+        }
+
+        private static MemoryStream GenerateStreamFromString(string value)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(value ?? ""));
+        }
+
+        private static void UploadToBlobs(string connectionString)
+        {
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
-            
+
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
             // Retrieve a reference to a container.
             CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
@@ -25,19 +41,55 @@ namespace ConsoleApp
             // Create the container if it doesn't already exist.
             container.CreateIfNotExists();
 
-            // Retrieve reference to a blob named "myblob".
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference("myblob");
-
-            // Create or overwrite the "myblob" blob with contents from a local file.
-            using (var stream = GenerateStreamFromString("this is some test blob content"))
+            for (var i = 0; i < 10; i++)
             {
-                blockBlob.UploadFromStream(stream);
+                // Retrieve reference to a blob named "myblob".
+
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference($"fakeDomain{i}.com");
+
+                // Create or overwrite the "myblob" blob with contents from a local file.
+                using (var stream = GenerateStreamFromString($"this is some test blob content {i}"))
+                {
+                    blockBlob.UploadFromStream(stream);
+                }
             }
         }
 
-        private static MemoryStream GenerateStreamFromString(string value)
+        private static void ListBlobs(string connectionString)
         {
-            return new MemoryStream(Encoding.UTF8.GetBytes(value ?? ""));
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+
+            // Loop over items within the container and output the length and URI.
+            foreach (IListBlobItem item in container.ListBlobs(null, false))
+            {
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+
+                    Console.WriteLine("Block blob of length {0}: {1}", blob.Properties.Length, blob.Uri);
+
+                }
+                else if (item.GetType() == typeof(CloudPageBlob))
+                {
+                    CloudPageBlob pageBlob = (CloudPageBlob)item;
+
+                    Console.WriteLine("Page blob of length {0}: {1}", pageBlob.Properties.Length, pageBlob.Uri);
+
+                }
+                else if (item.GetType() == typeof(CloudBlobDirectory))
+                {
+                    CloudBlobDirectory directory = (CloudBlobDirectory)item;
+
+                    Console.WriteLine("Directory: {0}", directory.Uri);
+                }
+            }
         }
     }
 }
